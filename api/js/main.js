4a3a638128d7f8864738eae60d5caec1307b3a12
AliceLin321 <ataechiml24@gmail.com>
2023-02-03 13:56:34 -0500

    alice lin

--- api/js/main.js
+++ api/js/main.js
@@ -2,7 +2,6 @@ import "../styles/style.css";
 const URL = "https://api.quotable.io/random";
 async function getData(URL) {
   const response = await fetch(URL);
-  const data = response.json();
-  console.log(data);
+  console.log(response);
 }
 getData(URL);
